e85026c364d3c2c7c57adec18e8550901bf6aacd
Microbob µ <microbob@is88.com>
2020-03-14 09:39:13 -0700

    problematic fancy incident angle management, mathf tesster script. Simplifying

--- Assets/Scripts/PlaneMirror/PlaneMirrorPointLight.cs
+++ Assets/Scripts/PlaneMirror/PlaneMirrorPointLight.cs
@@ -9,14 +9,20 @@ namespace PlaneMirror
         public int rays = 2;
 
         private Transform _lightTrans;
+        private GameObject _virtualImage;
+
+        private int _animDir = 1;
 
         // Start is called before the first frame update
         void Start()
         {
             //Init Variables
             _lightTrans = lightSource.transform;
+            _virtualImage = GameObject.CreatePrimitive(PrimitiveType.Sphere);
 
             _lightTrans.eulerAngles = new Vector3(-45, 0, 0);
+            _virtualImage.SetActive(false);
+            _virtualImage.transform.localScale = new Vector3(0.3f, 0.3f, 0.3f);
         }
 
         // Update is called once per frame
@@ -25,13 +31,24 @@ namespace PlaneMirror
             Vector3 rayDir = _lightTrans.forward;
             if (Physics.Raycast(_lightTrans.position, rayDir, out RaycastHit hit))
             {
-                Debug.DrawRay(_lightTrans.position, _lightTrans.forward * hit.distance, Color.cyan);
-                Debug.DrawRay(hit.point, hit.normal, Color.red);
+                // Used hit data
+                Vector3 mirrorNorm = hit.normal;
+                Vector3 rayHitPoint = hit.point;
+                float rayDistance = hit.distance;
 
-                float incidentAngle = 180 - Vector3.Angle(hit.normal, rayDir);
-                Vector3 exitDir = hit.normal;
+                // print("Hit distance: " + rayDistance);
+
+                // Show incident ray
+                Debug.DrawRay(_lightTrans.position, _lightTrans.forward * rayDistance, Color.cyan);
+
+                // Show mirror normal
+                Debug.DrawRay(rayHitPoint, mirrorNorm, Color.red);
+
+                // Calculate exit angle
+                float incidentAngle = 180 - Vector3.Angle(mirrorNorm, rayDir);
+                Vector3 exitDir = mirrorNorm;
                 Vector3 exitAngle = new Vector3(0, Mathf.Sin(incidentAngle * Mathf.Deg2Rad), 0);
-                if (hit.point.y < _lightTrans.position.y)
+                if (rayHitPoint.y < _lightTrans.position.y)
                 {
                     exitDir -= exitAngle;
                 }
@@ -40,14 +57,33 @@ namespace PlaneMirror
                     exitDir += exitAngle;
                 }
 
-                print(hit.normal + "; " + incidentAngle + "; " + exitDir);
+                // print(exitDir + ", " + exitDir.normalized);
 
-                Debug.DrawRay(hit.point, exitDir, Color.green);
-                Debug.DrawRay(hit.point, -exitDir * hit.distance, Color.yellow);
-                Instantiate(GameObject.CreatePrimitive(PrimitiveType.Sphere), hit.point + (-exitDir * hit.distance),
-                    Quaternion.identity);
-                
+                Debug.DrawRay(rayHitPoint, exitDir.normalized, Color.green);
+                Debug.DrawRay(rayHitPoint, -exitDir.normalized * hit.distance, Color.yellow, 3);
+
+                if (!_virtualImage.activeSelf)
+                {
+                    _virtualImage.SetActive(true);
+                }
+
+                _virtualImage.transform.position = rayHitPoint + -exitDir.normalized * hit.distance;
             }
+
+            float xAngle = _lightTrans.eulerAngles.x;
+            print(xAngle);
+            if (xAngle > 45)
+            {
+                _animDir = -1;
+                xAngle = 45;
+            }
+            else if (xAngle < -45)
+            {
+                _animDir = 1;
+                xAngle = -45;
+            }
+
+            _lightTrans.eulerAngles = new Vector3(xAngle + Time.deltaTime * 3 * _animDir, 0, 0);
         }
     }
 }
---
+++ Assets/Scripts/Primitive Labs/MathLabs.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+namespace Primitive_Labs
+{
+    public class MathLabs : MonoBehaviour
+    {
+        // Start is called before the first frame update
+        void Start()
+        {
+            float sin = Mathf.Sin(31 * Mathf.Deg2Rad);
+            print(sin);
+            Vector3 vect = new Vector3(sin, 0, 0);
+            print(vect.x);
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+        }
+    }
+}
