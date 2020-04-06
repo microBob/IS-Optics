cb996d9b6d4634a8e907f1d343bcd41298b40c67
Microbob µ <microbob@is88.com>
2020-04-05 23:06:18 -0700

    75% working plane mirror, need to debug bad normal calculations

--- Assets/Scripts/IncorporatedParticleImage.cs
+++ Assets/Scripts/IncorporatedParticleImage.cs
@@ -1,9 +1,12 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using Mirrors;
 using Refractions;
 using UnityEngine;
+
+// ReSharper disable AssignmentInConditionalExpression
 
 
 public enum Status
@@ -40,30 +43,34 @@ public class IncorporatedParticleImage : MonoBehaviour
     // Storage for render point seeking data and objects in scene
     private List<GameObject> _objectsInScene;
     private readonly List<Vector3> _objectPointsToRender = new List<Vector3>();
-    private GameObject _curSceneObject;
-    private RaycastHit _verifyPointSeek;
+    private GameObject _curTargetObject;
     private Vector3 _curInteractionPoint;
+    private RaycastHit _verifyPointSeek;
+    private LayerMask _sceneObjectLayerMask;
     private int _objectPointsIndex;
 
-    // Location services
+    // ID services
     private Vector3 _myPos;
+    private String _myName;
+    private int _generatedImageId;
 
     // Start is called before the first frame update
     void Start()
     {
+        _myPos = transform.position;
+        _myName = gameObject.name;
+        _sceneObjectLayerMask = LayerMask.GetMask("SceneObject");
     }
 
     // Update is called once per frame
     void Update()
     {
-        _myPos = transform.position;
-
         switch (_status)
         {
             case Status.SeekingPoints: // Look for points to render to
                 // Get all GameObjects with tag "SceneObject"
                 _objectsInScene = GameObject.FindGameObjectsWithTag("SceneObject").ToList();
-                print("number of objects: " + _objectsInScene.Count);
+                print(_myName + ": number of objects: " + _objectsInScene.Count);
 
                 // Iter through, temporarily change MeshCollider to convex to use ClosestPoint function to get approx closest point
                 foreach (GameObject o in _objectsInScene)
@@ -76,49 +83,108 @@ public class IncorporatedParticleImage : MonoBehaviour
                 // Move forward if there were actually objects in the scene
                 if (_objectPointsToRender.Count > 0)
                 {
+                    _objectPointsIndex = 0;
                     _status = Status.VerifyingPoints;
+                    print(_myName + ": Moving to Verifying status");
                 }
 
                 break;
             case Status.VerifyingPoints: // check that these points are physically valid
-                // Get a point from the list to work with
-                Vector3 point = _objectPointsToRender[_objectPointsIndex];
-                print(gameObject.name + " Verifying point: " + point);
+                // Get the appropriate data for the current object indexed
+                _curInteractionPoint = _objectPointsToRender[_objectPointsIndex];
+                _curTargetObject = _objectsInScene[_objectPointsIndex];
+                print(_myName + ": Verifying point: " + _curInteractionPoint);
 
                 // Raycast to see if this exact point is accessible from the object
-                bool dontRemoveFlag = Physics.Raycast(_myPos, (point - _myPos).normalized, out _verifyPointSeek,
-                    Mathf.Infinity, LayerMask.GetMask("SceneObject"));
+                bool dontRemoveFlag = Physics.Raycast(_myPos, (_curInteractionPoint - _myPos).normalized,
+                    out _verifyPointSeek,
+                    Mathf.Infinity, _sceneObjectLayerMask);
 
                 if (dontRemoveFlag)
                 {
                     // yes, this point on another object is accessible
+                    print(_myName + ": Found point " + _curInteractionPoint + " accessible");
+                    Debug.DrawRay(_myPos, (_curInteractionPoint - _myPos).normalized * _verifyPointSeek.distance,
+                        Color.blue);
                     switch (imageType)
                     {
                         case ImageType.OriginalObject:
-                            Debug.DrawRay(_myPos, (point - _myPos).normalized * _verifyPointSeek.distance, Color.blue);
-
-                            dontRemoveFlag =
-                                _verifyPointSeek.collider.gameObject.Equals(_objectsInScene[_objectPointsIndex]);
-                            if (dontRemoveFlag)
+                            print(_myName + ": is an original object");
+                            // check that the new point is the target object
+                            if (dontRemoveFlag =
+                                _verifyPointSeek.collider.gameObject.Equals(_curTargetObject))
                             {
                                 // Update points list to the exact point to render with
+                                print(_myName + ": Verified render point, moving to point index " +
+                                      (_objectPointsIndex + 1));
                                 _objectPointsToRender[_objectPointsIndex] = _verifyPointSeek.point;
                                 _objectPointsIndex++;
                             }
 
                             break;
                         case ImageType.MirrorVirtualImage:
-                            
+                            print(_myName + ": is a virtual image");
+                            // Exit early if target object is source mirror
+                            if (sourceSceneObject.Equals(_curTargetObject))
+                            {
+                                print(_myName + ": point " + _curInteractionPoint +
+                                      " is source mirror, moving to next");
+                                dontRemoveFlag = false;
+                                break;
+                            }
+
+                            // check that the new point's hit (will go through) the source mirror
+                            if (dontRemoveFlag = _verifyPointSeek.collider.gameObject.Equals(sourceSceneObject))
+                            {
+                                print(_myName + ": point " + _curInteractionPoint + " goes through source mirror");
+                                // Raycast a second time from source mirror surface (same target)
+                                Vector3 newSeekDir = (_curInteractionPoint - _verifyPointSeek.point).normalized;
+                                Vector3 newPushedVerifyPointseek = _verifyPointSeek.point + newSeekDir * 0.01f;
+                                if (dontRemoveFlag = Physics.Raycast(newPushedVerifyPointseek,
+                                    newSeekDir, out _verifyPointSeek,
+                                    Mathf.Infinity, _sceneObjectLayerMask))
+                                {
+                                    Debug.DrawLine(newPushedVerifyPointseek, _verifyPointSeek.point, Color.red,
+                                        Mathf.Infinity);
+                                    print(_myName + ": second raycast from " + newPushedVerifyPointseek + " to " +
+                                          _verifyPointSeek.point);
+                                    // Progress forward if the second cast made it to the target (no obstacles)
+                                    // 1) ray from source passes through source mirror
+                                    // 2) leaving source mirror goes directly to point
+                                    if (dontRemoveFlag =
+                                        _verifyPointSeek.collider.gameObject.Equals(
+                                            _curTargetObject))
+                                    {
+                                        print(_myName + ": point " + _curInteractionPoint +
+                                              " found actual hit point on target as virutal image");
+                                        _objectPointsToRender[_objectPointsIndex] = _verifyPointSeek.point;
+                                        _objectPointsIndex++;
+                                    }
+                                    else
+                                    {
+                                        print(_myName + ": point " + _curInteractionPoint +
+                                              " hit point after source mirror isn't target object. was expecting " +
+                                              _curTargetObject.name + " got " +
+                                              _verifyPointSeek.collider.gameObject.name);
+                                    }
+                                }
+                                else
+                                {
+                                    print(_myName + ": point " + _curInteractionPoint +
+                                          "nable to hit after passing source mirror");
+                                }
+                            }
+
                             break;
                     }
                 }
 
-                // Remove this point and object from the list (it not interacting with this particle)
+                // Remove this point and object from the list (it's not interacting with this particle)
                 if (!dontRemoveFlag)
                 {
+                    print(_myName + ": removing point " + _curInteractionPoint);
                     _objectPointsToRender.RemoveAt(_objectPointsIndex);
                     _objectsInScene.RemoveAt(_objectPointsIndex);
-                    _objectPointsIndex--;
                 }
 
                 // Once all points are handled, move on to rendering
@@ -126,6 +192,7 @@ public class IncorporatedParticleImage : MonoBehaviour
                 {
                     _objectPointsIndex = 0;
                     _status = Status.PreRendering;
+                    print(_myName + ": all points have been verified, moving to pre-rendering");
                 }
 
                 break;
@@ -133,37 +200,57 @@ public class IncorporatedParticleImage : MonoBehaviour
                 // Complete this object's job is it is done
                 if (_objectPointsIndex.Equals(_objectPointsToRender.Count))
                 {
+                    print(_myName + ": completed all points to render! Status is now COMPLETE");
                     _status = Status.Complete;
                     break;
                 }
 
                 // Get the appropriate data for the current object indexed
-                _curSceneObject = _objectsInScene[_objectPointsIndex];
+                _curTargetObject = _objectsInScene[_objectPointsIndex];
                 _curInteractionPoint = _objectPointsToRender[_objectPointsIndex];
+                print(_myName + ": Rendering point: " + _curInteractionPoint);
 
                 // Check the type of object, and render
-                if (_curSceneObject.GetComponent<PlaneMirrorDef>() != null)
+                if (_curTargetObject.GetComponent<PlaneMirrorDef>() != null)
                 {
+                    print(_myName + ": point " + _curInteractionPoint + " is a plane mirror! Rendering...");
                     _status = Status.PlaneMirrorRendering;
                 }
-                else if (_curSceneObject.GetComponent<SphericalMirrorDef>() != null)
+                else if (_curTargetObject.GetComponent<SphericalMirrorDef>() != null)
                 {
+                    print(_myName + ": point " + _curInteractionPoint + " is a spherical mirror! Rendering...");
                     _status = Status.SphericalMirrorRendering;
                 }
-                else if (_curSceneObject.GetComponent<ThinLensDef>() != null)
+                else if (_curTargetObject.GetComponent<ThinLensDef>() != null)
                 {
+                    print(_myName + ": point " + _curInteractionPoint + " is a thin lens! Rendering...");
                     _status = Status.ThinLensRendering;
                 }
                 else // WTF?
                 {
-                    print("Encountered unknown object: " + _curSceneObject.name);
-                    _curSceneObject.GetComponent<Renderer>().material.color = Color.red;
+                    print(_myName + ": Encountered unknown object: " + _curTargetObject.name);
+                    _curTargetObject.GetComponent<Renderer>().material.color = Color.red;
                 }
 
                 break;
             case Status.PlaneMirrorRendering:
-                PlaneMirrorRender(_myPos, _objectPointsToRender[_objectPointsIndex],
-                    _verifyPointSeek, out Vector3 outputImagePos);
+                print(_myName + ": rendering a plane mirror from " + _myPos + " to " + _curInteractionPoint +
+                      " on mirror " + _verifyPointSeek.collider.gameObject.name);
+
+                Vector3 mirrorNorm = _verifyPointSeek.normal;
+
+                Vector3 exitDir = Quaternion.AngleAxis(180, mirrorNorm) * -(_curInteractionPoint - _myPos).normalized;
+                
+                Debug.DrawLine(_myPos, _curInteractionPoint, Color.cyan, Mathf.Infinity);
+                Debug.DrawRay(_curInteractionPoint, mirrorNorm, Color.red, Mathf.Infinity);
+                Debug.DrawRay(_curInteractionPoint, exitDir, Color.green, Mathf.Infinity);
+                Debug.DrawRay(_curInteractionPoint, -exitDir * _verifyPointSeek.distance, Color.yellow, Mathf.Infinity);
+
+                Vector3 outputImagePos = _curInteractionPoint - exitDir * _verifyPointSeek.distance;
+
+
+                // PlaneMirrorRender(_myPos, _curInteractionPoint,
+                //     _verifyPointSeek, out Vector3 outputImagePos);
 
                 // GameObject image = GameObject.CreatePrimitive(PrimitiveType.Sphere);
                 // image.transform.position = outputImagePos;
@@ -171,16 +258,24 @@ public class IncorporatedParticleImage : MonoBehaviour
                 //
                 // _status = Status.Idle;
 
+                print(_myName + ": Found image location at " + outputImagePos);
+
                 GameObject image = Instantiate(Resources.Load<GameObject>("Objects/ParticleImagePoint"), outputImagePos,
                     Quaternion.identity);
+                image.name = _myName + " (Virtual image) " + _generatedImageId;
+                _generatedImageId++;
 
                 IncorporatedParticleImage incorporatedParticleImage = image.GetComponent<IncorporatedParticleImage>();
                 incorporatedParticleImage.imageType = ImageType.MirrorVirtualImage;
-                incorporatedParticleImage.sourceSceneObject = _objectsInScene[_objectPointsIndex];
+                incorporatedParticleImage.sourceSceneObject = _curTargetObject;
+
+                print(_myName + ": created virtual image of " + _curTargetObject.name + " with name " + image.name);
 
                 // Return to prerendering for the next point
                 _objectPointsIndex++;
                 _status = Status.PreRendering;
+                print(_myName + ": moving to work on point index " + _objectPointsIndex +
+                      ". going back to prerendering");
 
                 break;
             case Status.SphericalMirrorRendering:
@@ -215,7 +310,7 @@ public class IncorporatedParticleImage : MonoBehaviour
         //     }
         // } while (hit.collider.gameObject != targetingObject);
 
-        print("hitData Mirror " + hitData.collider.gameObject.name);
+        print(_myName + ": rendering with hitData Mirror " + hitData.collider.gameObject.name);
 
         Vector3 mirrorNorm = hitData.normal;
 
--- Assets/Scripts/Raycast Labs/RaycastLabs.cs
+++ Assets/Scripts/Raycast Labs/RaycastLabs.cs
@@ -1,25 +1,59 @@
-﻿using System.Collections;
-using System.Collections.Generic;
+﻿using System;
 using UnityEngine;
 
-public class RaycastLabs : MonoBehaviour
+namespace Raycast_Labs
 {
-    public PointLight lightSource;
-    // Start is called before the first frame update
-    void Start()
+    public class RaycastLabs : MonoBehaviour
     {
-        if (Physics.Raycast(lightSource.transform.position,
-            (transform.position - lightSource.transform.position).normalized, out RaycastHit hit, Mathf.Infinity))
-        {
-            Debug.DrawRay(lightSource.transform.position,
-                (transform.position - lightSource.transform.position).normalized * hit.distance, Color.cyan,Mathf.Infinity);
-            hit.collider.gameObject.GetComponent<Renderer>().material.color = Color.red;
-        }
-    }
+        public GameObject targetObject;
+        public GameObject secondObject;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+        private Vector3 _myPos;
+
+        private Vector3 _direction;
+        private Vector3 _lastDir;
+        private int _layerMask;
+        private RaycastHit _hit;
+        private int _runs;
+        private bool _once;
+
+        // Start is called before the first frame update
+        void Start()
+        {
+            _myPos = transform.position;
+
+
+            _direction = (secondObject.transform.position - _myPos).normalized;
+            _layerMask = LayerMask.GetMask("SceneObject");
+
+            print(_direction);
+            _once = false;
+            if (Physics.Raycast(_myPos,
+                _direction, out _hit, Mathf.Infinity, _layerMask))
+            {
+                Debug.DrawLine(_myPos, _hit.point, Color.green, Mathf.Infinity);
+
+                if (Physics.Raycast(_hit.point,_direction,out RaycastHit secondHit,Mathf.Infinity,_layerMask))
+                {
+                    Debug.DrawLine(_hit.point, secondHit.point, Color.red, Mathf.Infinity);
+
+                    _once = secondHit.collider.gameObject.Equals(targetObject);
+                }
+            }
+
+            if (_once)
+            {
+                print("Failed");
+            }
+            else
+            {
+                print("made it through");
+            }
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+        }
     }
 }
