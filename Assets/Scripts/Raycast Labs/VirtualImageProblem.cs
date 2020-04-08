8f43db02092238bac921e258ab5e9349d0cfbca8
Microbob µ <microbob@is88.com>
2020-04-07 19:48:12 -0700

    calculate the field of view of a virtual object from a mirror, working to find interaction point

--- Assets/Scripts/Raycast Labs/VirtualImageProblem.cs
+++ Assets/Scripts/Raycast Labs/VirtualImageProblem.cs
@@ -1,4 +1,7 @@
-﻿using UnityEngine;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
 
 namespace Raycast_Labs
 {
@@ -41,44 +44,115 @@ namespace Raycast_Labs
         // Update is called once per frame
         void Update()
         {
-            switch (_status)
+            if (_status == Status.SeekingSource)
             {
-                case Status.SeekingSource:
-                    if (Physics.Raycast(_seekOrigin, _direction, out RaycastHit toSourceMirrorHit,
-                        Mathf.Infinity))
+                // SECTION: get border verts
+                Mesh sourceMirrorMesh = sourceMirror.GetComponent<MeshFilter>().mesh;
+                Vector3[] verts = sourceMirrorMesh.vertices;
+
+                print("Full list of Verts (" + verts.Length + "):\n");
+                VertListToString(verts);
+
+                // SECTION: only unique
+                verts = verts.ToList().Distinct().ToArray();
+
+                print("Only unique verts (" + verts.Length + "):\n");
+                VertListToString(verts);
+
+                // SECTION: convert to world space
+                for (int i = 0; i < verts.Length; i++)
+                {
+                    verts[i] = sourceMirror.transform.TransformPoint(verts[i]);
+                }
+
+                print("Transformed unique verts (" + verts.Length + "):\n");
+                VertListToString(verts);
+
+                // SECTION: Debug with rays
+                // foreach (Vector3 vert in verts)
+                // {
+                //     Debug.DrawRay(_myPos, (vert - _myPos).normalized * 15, Color.cyan, Mathf.Infinity);
+                // }
+
+                // SECTION: create seek mesh verts, set 0 -> myPos
+                Vector3[] newVerts = new Vector3[verts.Length + 1];
+
+                newVerts[0] = _myPos;
+
+                for (int i = 1; i < newVerts.Length; i++)
+                {
+                    newVerts[i] = _myPos + (verts[i - 1] - _myPos).normalized * 15;
+                }
+
+                print("New mesh verts:");
+                VertListToString(newVerts);
+
+                // SECTION: Debug new verts with lines
+                // foreach (Vector3 vert in newVerts)
+                // {
+                //     Debug.DrawLine(_myPos, vert, Color.magenta, Mathf.Infinity);
+                // }
+
+                // SECTION: assign triangles from newVerts
+                int[] tris = new int[(newVerts.Length - 1) * 3];
+                for (int i = 0; i < newVerts.Length - 1; i++)
+                {
+                    tris[i * 3] = i + 1;
+
+                    if (i + 2 == newVerts.Length)
                     {
-                        _seekOrigin = toSourceMirrorHit.point;
-                        if (toSourceMirrorHit.collider.gameObject.Equals(sourceMirror))
-                        {
-                            _status = Status.SeekingTarget;
-                        }
+                        tris[i * 3 + 1] = 1;
+                    }
+                    else
+                    {
+                        tris[i * 3 + 1] = i + 2;
                     }
 
-                    _seekOrigin += _direction * 0.001f;
+                    tris[i * 3 + 2] = 0;
+                }
 
-                    break;
-                case Status.SeekingTarget:
-                    if (Physics.Raycast(_seekOrigin, _direction, out RaycastHit toTargetHit, Mathf.Infinity))
-                    {
-                        Color msgColor = Color.green;
-                        if (toTargetHit.collider.gameObject.Equals(targetObejct))
-                        {
-                            _status = Status.CompletedSuccess;
-                        }
-                        else
-                        {
-                            _status = Status.CompletedFailed;
-                            msgColor = Color.red;
-                        }
+                print("Tris list:");
+                IntListToString(tris);
 
-                        Debug.DrawLine(_myPos, toTargetHit.point, msgColor, Mathf.Infinity);
-                        print("Completed with status: " + _status);
-                    }
+                // SECTION: create gameobject from verts and tris
+                GameObject seekObject = new GameObject(gameObject.name + "VirtualImageSeekCast");
+                Mesh seekMesh = new Mesh();
 
-                    break;
-                default:
-                    break;
+                seekMesh.vertices = newVerts;
+                seekMesh.triangles = tris;
+                seekMesh.RecalculateNormals();
+                seekMesh.RecalculateBounds();
+
+                // seekObject.AddComponent<MeshRenderer>();
+
+                MeshFilter meshFilter = seekObject.AddComponent<MeshFilter>();
+                meshFilter.mesh = seekMesh;
+
+                MeshCollider meshCollider = seekObject.AddComponent<MeshCollider>();
+                meshCollider.convex = true;
+                
+                _status = Status.CompletedSuccess;
             }
+        }
+
+        private void VertListToString(Vector3[] list)
+        {
+            foreach (Vector3 vert in list)
+            {
+                print("(" + vert.x + ", " + vert.y + ", " + vert.z + ")\n");
+            }
+
+            print("\n");
+        }
+
+        private void IntListToString(int[] list)
+        {
+            foreach (int i in list)
+            {
+                print(i + "\n");
+            }
+
+            print("\n");
         }
     }
 }
