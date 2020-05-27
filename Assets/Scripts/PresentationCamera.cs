e01cb57cb988988bfa1d485b8a0f20b8b99d8dc2
Kenneth Y <kyang@eastsideprep.org>
2020-05-27 09:35:34 -0700

    working on thin lens, checking out past work

--- Assets/Scripts/PresentationCamera.cs
+++ Assets/Scripts/PresentationCamera.cs
@@ -1,5 +1,7 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
+using UnityEditor.Graphs;
 using UnityEngine;
 
 public enum CamStage
@@ -12,21 +14,20 @@ public class PresentationCamera : MonoBehaviour
 {
     public float flySpeed = 2f;
 
-    public float orbitRadius = 10f;
-
     public Camera mainCamera;
 
     private Transform _myTrans;
     private Vector3 _myPos;
     private CamStage _myCamStage;
 
-    private float _timeCounter;
+    private float _defaultDistance;
 
     // Start is called before the first frame update
     void Start()
     {
         _myTrans = transform;
         _myCamStage = CamStage.Static;
+        _defaultDistance = _myTrans.position.z;
     }
 
     // Update is called once per frame
@@ -34,13 +35,16 @@ public class PresentationCamera : MonoBehaviour
     {
         float horizInput = Input.GetAxis("Horizontal");
         float vertInput = Input.GetAxis("Vertical");
+        float yVertInput = Input.GetAxis("YVertical");
 
         _myCamStage = Input.GetButton("Jump") ? CamStage.HorizontalOrbit : CamStage.Static;
 
         if (Input.GetKey(KeyCode.R))
         {
             _myTrans.rotation = Quaternion.Euler(Vector3.zero);
+            _myTrans.position = new Vector3(0, 0, _defaultDistance);
         }
+
 
         switch (_myCamStage)
         {
@@ -49,14 +53,18 @@ public class PresentationCamera : MonoBehaviour
                 {
                     mainCamera.orthographic = true;
                 }
+
+                mainCamera.orthographicSize -= yVertInput * Time.deltaTime;
+
                 break;
             case CamStage.HorizontalOrbit:
                 if (mainCamera.orthographic)
                 {
                     mainCamera.orthographic = false;
                 }
-                
-                _myTrans.Rotate(Vector3.up, horizInput * Time.deltaTime * flySpeed, Space.World);
+
+                _myTrans.Translate(0, 0, yVertInput * Time.deltaTime);
+                _myTrans.Rotate(Vector3.up, -horizInput * Time.deltaTime * flySpeed, Space.World);
                 _myTrans.Rotate(Vector3.right, vertInput * Time.deltaTime * flySpeed);
                 break;
             default:
